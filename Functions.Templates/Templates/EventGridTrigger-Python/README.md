5036dc7abfd84981f5911b371ef8881aeacffb8d
t-eckert <t_eckert@outlook.com>
2019-06-21 20:50:34 -0700

    Add README Access every element of Class

---
+++ Functions.Templates/Templates/EventGridTrigger-Python/README.md
@@ -0,0 +1,1 @@
+# Event Grid Trigger
--- Functions.Templates/Templates/EventGridTrigger-Python/__init__.py
+++ Functions.Templates/Templates/EventGridTrigger-Python/__init__.py
@@ -1,16 +1,16 @@
-import json
 import logging
+from datetime import datetime
 
 import azure.functions as func
 
 
 def main(event: func.EventGridEvent):
-    result = json.dumps({
-        'id': event.id,
-        'data': event.get_json(),
-        'topic': event.topic,
-        'subject': event.subject,
-        'event_type': event.event_type,
-    })
 
-    logging.info('Python EventGrid trigger processed an event: %s', result)
+    event_id: str = event.id
+    event_data: dict = event.get_json()
+    event_topic: str = event.topic
+    event_subject: str = event.subject
+    event_type: str = event.event_type
+    event_time: datetime = event.event_time
+    event_data_version: str = event.event_data_version
+
